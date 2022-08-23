0603a6b196575260974ac305d438a9c8592b76c2
Martins Passos <matheus.passos@externos-br.cencosud.com>
2022-08-23 11:52:49 -0300

    Criado as funções de soma e subtracao

--- Program.cs
+++ Program.cs
@@ -7,10 +7,33 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
+            Soma();
+            Subtracao();
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
             Console.Write("Primeiro valor: ");
             float a = float.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            float b = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(a);
+            Console.WriteLine("--------------");
+            float resultado = a + b;
+            Console.WriteLine("O resultado da soma é: " + resultado);
+        }
+
+        static void Subtracao()
+        {
+            Console.Write("Primeiro valor: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            float b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("--------------");
+            float resultado = a - b;
+            Console.WriteLine("O resultado da subtração é: " + resultado);
         }
     }
 }
