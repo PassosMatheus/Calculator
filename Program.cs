fa9bf1f995b5db7a9832bdd0e84de73f71015d5f
Martins Passos <matheus.passos@externos-br.cencosud.com>
2022-08-23 11:20:33 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Primeiro valor: ");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(a);
+        }
+    }
+}
